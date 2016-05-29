be23e80a85f3fb4b11136375b6de03707bcc4297
Jeroen Stravers <jeroen.stravers@live.nl>
2016-05-29 11:14:48 +0200

    added uploading images as base64 string json content with post to ubuntu server, test version

--- datacollector/DataCollector.py
+++ datacollector/DataCollector.py
@@ -1,12 +1,11 @@
 import cv2
 import time
 import os
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+import requests
+import json
 from random import randint
 
 class HeadsUpDataCollector():
-    _API_KEY                        = "575282054454-5lhuo215eq5b8doe42i9psfbnu3ttfh0.apps.googleusercontent.com"
     _DRIVE_DATA_FOLDERNAME          = "HUDC"
     _DESIRED_NUMBER_OF_SNAPSHOTS    = 30
     _INITIAL_SNAPSHOT_DELAY         = 30
@@ -20,19 +19,6 @@ class HeadsUpDataCollector():
 
     def __init__(self):
         self._snapshot_count = 0
-        '''
-        self._gauth = GoogleAuth()
-        self._gauth.LoadCredentialsFile("mycreds.txt")
-        #self._gauth.credentials = '{"_module": "oauth2client.client", "scopes": ["https://www.googleapis.com/auth/drive"], "token_expiry": "2016-04-27T14:15:08Z", "id_token": null, "access_token": "ya29.CjHRAjA4A1ZedtXQl7lHRemkHQvcXQnAIJ1rqm7TsGHP8EvHrEBmwKkNzenqHFIUY7DL", "token_uri": "https://accounts.google.com/o/oauth2/token", "invalid": false, "token_response": {"access_token": "ya29.CjHRAjA4A1ZedtXQl7lHRemkHQvcXQnAIJ1rqm7TsGHP8EvHrEBmwKkNzenqHFIUY7DL", "token_type": "Bearer", "expires_in": 3599}, "client_id": "1026924012842-2n7oj3euf07qqdqnguf6bup5mndqlmgn.apps.googleusercontent.com", "token_info_uri": "https://www.googleapis.com/oauth2/v3/tokeninfo", "client_secret": "_LZHE0IitzzIZoHD0BMbPo5g", "revoke_uri": "https://accounts.google.com/o/oauth2/revoke", "_class": "OAuth2Credentials", "refresh_token": null, "user_agent": null}'
-        if self._gauth.credentials is None:
-            self._gauth.LocalWebserverAuth()
-        elif self._gauth.access_token_expired:
-            self._gauth.Refresh()
-        else:
-            self._gauth.Authorize()
-        self._gauth.SaveCredentialsFile("mycreds.txt")
-        self._drive = GoogleDrive(self._gauth)
-        self._id = self.getParentFolderId()'''
         pass
 
     def getParentFolderId(self):
@@ -55,9 +41,9 @@ class HeadsUpDataCollector():
         time.sleep(self._INITIAL_SNAPSHOT_DELAY)
         while self._snapshot_count < self._DESIRED_NUMBER_OF_SNAPSHOTS:
             snapshot_successful, snapshot_filename = self.takeSnaphot()
-            '''if snapshot_successful:
+            if snapshot_successful:
                 self.uploadSnapshot(snapshot_filename)
-                self.deleteOldSnapshot(snapshot_filename)'''
+                self.deleteOldSnapshot(snapshot_filename)
             time.sleep(self._SNAPSHOT_INTERVAL)
 
     def takeSnaphot(self):
@@ -79,15 +65,16 @@ class HeadsUpDataCollector():
             return True, snapshot_filename
         return False, self._SNAPSHOT_FAILED
 
-    def uploadSnapshot(self, filename):
+    def uploadSnapshot(self, filename_with_path):
         """
         uploads a snapshot to Heads Up's Google Drive account for
         further processing.
         :param filename: the name of the snapshot file to upload
         """
-        image_file = self._drive.CreateFile({'title': filename, "parents":  [{"kind": "drive#fileLink","id": self._id}]})
-        image_file.SetContentFile(filename)
-        image_file.Upload()
+        with open(filename_with_path, "rb") as f:
+            data = f.read()
+            base64_image = data.encode("base64")
+            r = requests.post('145.24.222.162:8000/data', json={"image_datauri": base64_image})
 
     def deleteOldSnapshot(self, filename):
         os.remove(filename)
