b9cc279902e648b36bc77aff27027f73ff88b49f
Jeroen Stravers <jeroen.stravers@live.nl>
2016-05-30 11:02:12 +0200

    updated snapshot interval values for actual data collection, removed testing-related images

--- datacollector/DataCollector.py
+++ datacollector/DataCollector.py
@@ -4,24 +4,26 @@ import os
 import requests
 import json
 from random import randint
+import base64
 
 
 class HeadsUpDataCollector():
 
     _DRIVE_DATA_FOLDERNAME          = "HUDC"
-    _DESIRED_NUMBER_OF_SNAPSHOTS    = 3#30
-    _INITIAL_SNAPSHOT_DELAY         = 3#30
+    _DESIRED_NUMBER_OF_SNAPSHOTS    = 1#30
+    _INITIAL_SNAPSHOT_DELAY         = 1#30
     _SNAPSHOT_FAILED                = ""
     _SNAPSHOT_FILETYPE              = ".jpg"
     _SNAPSHOT_FOLDER                = "Heads_Up_data/"
-    _SNAPSHOT_INTERVAL              = 5#15
+    _SNAPSHOT_INTERVAL              = 1#15
     _SNAPSHOT_TITLE                 = "Heads_Up_data_collection_snapshot"
     _JSON_KEY                       = "image_datauri"
     _UID                            = "uid"
+    _URL                            = "http://145.24.222.162:8000/data"
 
     def __init__(self):
         self._snapshot_count = 0
-        self._uid = str(time.time) + str(randint(0, 999999))
+        self._uid = str(time.time()) + str(randint(0, 999999))
 
     def collect_data_with_interval(self):
         """
@@ -29,13 +31,16 @@ class HeadsUpDataCollector():
         if they were taken succesfully, repeats this proces every N
         seconds.
         """
-        os.mkdir("Heads_Up_data", 777)
+        try:
+            os.mkdir("Heads_Up_data", 777)
+        except OSError as ose:
+            pass
         time.sleep(self._INITIAL_SNAPSHOT_DELAY)
         while self._snapshot_count < self._DESIRED_NUMBER_OF_SNAPSHOTS:
             snapshot_successful, snapshot_filename = self._take_snaphot()
             if snapshot_successful:
                 self._upload_snapshot(snapshot_filename)
-                self._delete_old_snapshot(snapshot_filename)
+                #self._delete_old_snapshot(snapshot_filename)
             time.sleep(self._SNAPSHOT_INTERVAL)
 
     def _take_snaphot(self):
@@ -49,7 +54,7 @@ class HeadsUpDataCollector():
         if successful:  # frame captured without any errors
             timestamp = str(time.time())
             random_digits = str(randint(11110,99999))
-            snapshot_filename = self._SNAPSHOT_FOLDER+ timestamp \
+            snapshot_filename = self._SNAPSHOT_FOLDER + timestamp \
                                 + self._SNAPSHOT_TITLE \
                                 + random_digits + self._SNAPSHOT_FILETYPE
             cv2.imwrite(snapshot_filename, snapshot)
@@ -64,9 +69,32 @@ class HeadsUpDataCollector():
         :param filename: the name of the snapshot file to upload
         """
         with open(filename_with_path, "rb") as f:
-            data = f.read()
-            base64_image = data.encode("base64")
-            r = requests.post('145.24.222.162:8000/data', json={self._JSON_KEY: base64_image, self._UID: self._uid})
+            base64_image = base64.b64encode(f.read())
+            r = requests.post(self._URL, json={self._JSON_KEY: base64_image, self._UID: self._uid})
+            decoded_image = self._decode_datauri(filename_with_path, base64_image)
+            self._save_image_in_folder(decoded_image, str(self._snapshot_count))
+
+    def _decode_datauri(self, filename_with_path, base64_image):
+        print filename_with_path
+        print base64_image
+        fh = open("x.jpg", 'wb')
+        fh.write(base64_image.decode('base64'))
+        fh.close()
+
+        """
+        with open(filename_with_path, "rb") as f:
+            image = base64.b64encode(f.read()).decode()
+            timestamp = time.time()
+            image_name = (str(timestamp) + ".jpg")
+            return image, image_name"""
+
+    def _save_image_in_folder(self, image, filename):
+        with open(filename, 'wb') as image_on_disk:
+            try:
+                image_on_disk.write(image)
+            except Exception as write_exception:
+                print str(write_exception) + "_save_image_in_folder"
+                pass  # TODO - logging insert
 
     def _delete_old_snapshot(self, filename):
         try:
@@ -77,5 +105,5 @@ class HeadsUpDataCollector():
 
 if __name__ == "__main__":
     data_collector = HeadsUpDataCollector()
-    data_collector.collectDataWithInterval()
+    data_collector.collect_data_with_interval()
 
